0724e7f89a873677153378e7ad1fa2e1300df2d4
kamchamswarna <kamchamswarna@gmail.com>
2020-11-10 19:30:58 +0530

    tictactoe game upto usecase 4

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -3,11 +3,14 @@ package tictoegame;
 import java.util.Scanner;
 
 public class TicTacToeGame {
-	private static final int O = 0;
-	static char[] Board = new char[10];
 	
-	public static char[] gameBoard() {
-		for(int i=0;i<=Board.length;i++) {  
+	static char[] Board = new char[10];
+	static char player = ' ';
+	static char computer = ' ';
+	
+	
+	public static char[] initializeBoard() {
+		for(int i=1;i>=Board.length;i++) {  
 			 Board[i]=' '; 	
 		}
 		return Board;
@@ -15,30 +18,76 @@ public class TicTacToeGame {
 	
 	 
 	public static char chooseChoice() {
-		char player = 'X';
+		
+		
 		Scanner choice = new Scanner(System.in);
-		while(true) {
+		
+			
 			System.out.println("select your choice X or O");
 			player = choice.next().toUpperCase().charAt(0);
 			
-			if(player == 'X' || player == 'O') {
+			if(player == 'X') {
+				player = 'x';
+				computer = 'o';
+			
+			}
+			else if(player == 'O') {
+				player = 'o';
+				computer = 'x';
+			
+			}
+			
+			else 
+			
+				System.out.println("you entered invalid choice");
+			
+			return 0;
+		}
+		
+		
+	
+	
+	public static void input() {
+		Scanner input = new Scanner(System.in);
+		while(true) {
+			
+			System.out.println("enter value in between 1 to 9 ");
+			int value = input.nextInt();
+			if(value >= 1 && value <= 9) {
+				if(Board[value] == ' ') {
+					Board[value]=player;
 				
-			return player;
+				}
+				else {
+					System.out.println("place is already filled");
+				}
+		
 			}
 			else {
-				System.out.println("you entered invalid choice");
-				continue;
+				System.out.println("you entered invalid value");
 			}
+	
+		}
+		
+	}
+	
+	public static void showBoard() {
+		
+		for(int i=1;i<=3;i++) {
+			
+			System.out.println("|" +Board[i] + "|" +Board[i+1] + "|" +Board[i+2]);
+			System.out.println("------------");
 			
 		}
 	}
 	
-	
 	public static void main(String[] args){
 		
 		System.out.println("Welcome to the tictactoe game");
-		gameBoard();
+		initializeBoard();
+		showBoard();
 		chooseChoice();
+		input();
 	}
 
 		
